d0f181a7454049d330edea04f80b49973fbf0fb2
Luis Guilherme Polippo <luispolippo@hotmail.com>
2021-03-12 22:47:19 -0300

    finished implementation of methods in Adapter and final implements in MainActivity

--- app/src/main/java/com/polippo/applicationworkapi/MainActivity.kt
+++ app/src/main/java/com/polippo/applicationworkapi/MainActivity.kt
@@ -2,7 +2,14 @@ package com.polippo.applicationworkapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.polippo.applicationworkapi.api.MyRetrofit
+import com.polippo.applicationworkapi.model.Product
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +20,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         recycleProducts = findViewById(R.id.recycler_products)
+        recycleProducts.layoutManager = LinearLayoutManager(this)
+        getData()
+    }
+
+    private fun getData(){
+        val call: Call<List<Product>> = MyRetrofit.instance?.productApi()?.getProductApi() as Call<List<Product>>
+        call.enqueue(object: Callback<List<Product>>{
+
+            override fun onResponse(call: Call<List<Product>>, response: Response<List<Product>>) {
+                val adapter = ProductAdapter(this@MainActivity, response.body()?.toList() as List<Product>)
+                recycleProducts.adapter = adapter
+            }
+
+            override fun onFailure(call: Call<List<Product>>, t: Throwable) {
+                Toast.makeText(this@MainActivity, t.message, Toast.LENGTH_LONG).show()
+            }
+
+        })
     }
 }
